f74a57b9f44052509c15fe5eab90b4a53de46cae
Anatolii Zorin <Zorger27@GMail.com>
2024-12-19 02:15:32 +0200

    Small changes

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -26,12 +26,12 @@ export default {
     <p class="footer-text">{{ $t('footer.text1') }}
       <b><a href="https://zorger27.github.io" title="CV (Landing Page)" target="_blank">{{ $t('footer.text2') }}
       </a></b>{{ $t('footer.text3') }}
-      <b>&copy; {{ currentYear }}</b> - {{ $t('footer.text4') }}
+      <b>&copy; 2024―{{ currentYear }}</b> - {{ $t('footer.text4') }}
     </p>
     <p class="footer-text-mob">{{ $t('footer.text1') }}
       <b><a href="https://zorger27.github.io" title="CV (Landing Page)" target="_blank">{{ $t('footer.text2') }}
       </a></b>{{ $t('footer.text3') }}<br>
-      <b>&copy; {{ currentYear }}</b> - {{ $t('footer.text4') }}
+      <span class="copyright"><b>&copy; 2023―{{ currentYear }}</b> - {{ $t('footer.text4') }}</span>
     </p>
     <SocialSharing></SocialSharing>
   </footer>
@@ -90,6 +90,7 @@ footer {
       font-size: 0.8rem;
       font-style: italic;
       color: saddlebrown;
+      .copyright {font-size: 0.65rem;}
     }
   }
 }
